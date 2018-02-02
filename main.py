e22d615ad1342486fa55b7af2b7c66b97d780f41
Bohyun <bhsuhkr@naver.com>
2018-02-01 20:40:52 -0600

    reward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 with open('bar_graph_reward.txt') as fp:
     line = fp.readline()
-    cnt = 1
     sum = 0
     episode = 0
     while line:
@@ -11,6 +10,5 @@ with open('bar_graph_reward.txt') as fp:
             sum += int(iteration)
             episode += 1
         line = fp.readline()
-        cnt += 1
     averageIter = float(sum / episode)
     print("The average iteration: " + format(averageIter))
