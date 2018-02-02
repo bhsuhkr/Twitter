b8b70ab0528250c9ee006537c87621ecb3ff3be0
bhsuhkr <bhsuhkr>
2018-02-01 18:52:58 -0600

    python

---
+++ main.py
@@ -0,0 +1,17 @@
+with open('bar_graph.txt') as fp:
+    line = fp.readline()
+    cnt =1
+    sum = 0
+    episode = 0
+    while line:
+        block1, block2, block3 = line.split(" , ")
+
+        if("ends" in block2):
+            letter, iteration = block2.split("= ")
+            sum += int(iteration)
+            episode += 1
+        line = fp.readline()
+        cnt +=1
+    averageIter = float(sum/episode)
+    print ("The average iteration: " + format(averageIter))
+
